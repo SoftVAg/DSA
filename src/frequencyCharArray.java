a34095d120e39aa5cb50e8feafff8d34e7e84b3e
Vedansh Agarwal <157943805+SoftVAg@users.noreply.github.com>
2024-04-27 17:38:56 +0530

    Add files via upload

---
+++ src/frequencyCharArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class frequencyCharArray {
+    public static void main(String[] args) {
+        String s = "I am learning coding";
+        int[] arr = new int[26];
+        s = s.toLowerCase();
+        for(int i =0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetter(ch)) {
+                arr[ch - 'a'] += 1;
+            }
+        }
+        for(int i =0; i<26; i++){
+            if(arr[i]!=0){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
