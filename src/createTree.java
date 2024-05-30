1b2aa7e5917f170553e3db2b9e735a3c116c3511
Vedansh Agarwal <157943805+SoftVAg@users.noreply.github.com>
2024-05-30 17:50:41 +0530

    Update createTree.java

--- src/createTree.java
+++ src/createTree.java
@@ -12,32 +12,78 @@ public class createTree {
          root.right = createTreee();
          return root;
     }
+     static ArrayList<Integer> topView(Node root) {
+        Queue<Pair> q = new ArrayDeque<>();
+        Map<Integer, Integer> map = new TreeMap<>();
+        q.add(new Pair(0, root));
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
+            if (!map.containsKey(curr.hd)) {
+                map.put(curr.hd, curr.node.data);
+            }
+            if (curr.node.left != null)
+                q.add(new Pair(curr.hd - 1, curr.node.left));
+            if (curr.node.right != null)
+                q.add(new Pair(curr.hd + 1, curr.node.right));
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            list.add(entry.getValue());
+        }
+        return list;
+    }
+     
+     static ArrayList<Integer> topView(Node root) {
+        Queue<Pair> q = new ArrayDeque<>();
+        Map<Integer, Integer> map = new TreeMap<>();
+        q.add(new Pair(0, root));
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
+            map.put(curr.hd, curr.node.data);
+            
+            if (curr.node.left != null)
+                q.add(new Pair(curr.hd - 1, curr.node.left));
+            if (curr.node.right != null)
+                q.add(new Pair(curr.hd + 1, curr.node.right));
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            list.add(entry.getValue());
+        }
+        return list;
+    }
+     
     static void inOrder(Node root){
          if(root==null) return;
          inOrder(root.left);
         System.out.print(root.data);
         inOrder(root.right);
     }
+     
     static void preOrder(Node root){
         if(root==null) return;
         System.out.print(root.data);
         preOrder(root.left);
         preOrder(root.right);
     }
+     
     static void postOrder(Node root){
         if(root==null) return;
         postOrder(root.left);
         postOrder(root.right);
         System.out.print(root.data);
     }
+     
     static int height(Node root){
          if(root==null) return 0;
          return Math.max(height(root.left),height(root.right))+1;
     }
+     
     static int size(Node root){
          if(root==null) return 0;
          return size(root.left)+size(root.right)+1;
     }
+     
     static Scanner sc = null;
     public static void main(String[] args) {
         sc = new Scanner (System.in);
@@ -49,5 +95,8 @@ public class createTree {
 //        postOrder(root);
         //System.out.println(height(root));
         System.out.println(size(root));
+         System.out.println(topView(root));
+         System.out.println(bottomView(root));
     }
+     
 }
