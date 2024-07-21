7feb0a28ac93866702fc11a20608c7452f1424cb
Vedansh Agarwal <157943805+SoftVAg@users.noreply.github.com>
2024-07-21 18:21:29 +0530

    Update bubbleSort.java

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -1,21 +1,21 @@
 import java.util.*;
-public class bubbleSort {
-    public static void main(String[] args) {
-        //Arraylist<Integer> arr = new ArrayList<>();int n = 5;
-        int[] arr = {4,1,3,2};
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length-1; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+public class bubblSort{
+    public static int[] bubbleSorts(int[] arr){
+        int n=arr.length;
+        for(int i=0; i<n; i++){
+            for(int j=1; j<n-i; j++){
+                if(arr[j-1]>arr[j]){
+                    int temp= arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j] =temp;
                 }
-//                System.out.println("runs");
             }
-            System.out.println("runs");
         }
-
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        arr = bubbleSorts(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
